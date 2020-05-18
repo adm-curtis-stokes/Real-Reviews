b52b26aefbe84b91521ab9feabb3cd016ea183ac
Lewis Morgans <lmorgans90@gmail.com>
2020-05-18 19:02:59 +0100

    initial set up

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index-page/index-component.component';
+
+export const AppRouteModule: Routes = [
+  { path: '', component: IndexComponent },
+
+]
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index-page/index-component.component';
+import { RouterModule } from '@angular/router';
+import { AppRouteModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRouteModule),
   ],
   providers: [],
   bootstrap: [AppComponent]
